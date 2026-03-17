0f0bc7a845035fa2bb7eab73fea5121c6a61e7a6
Bobrowski <bobrowski@tamu.edu>
2026-03-17 15:48:12 -0500

    test

---
+++ hexagon.py
@@ -0,0 +1,5 @@
+# This code comuputes the pase function and degree of linear polarization for unplarized incoident light 
+# scattered by a large hexagonal ice crystal under the random orientatio condition on the simplified 
+# 2D case discussed in class (refractive index m = 1.33, the maximum diomension of the particle = 2a, 
+# and size parameter x = 500)
+
---
+++ sphere.py
@@ -0,0 +1,3 @@
+# This code computes the phase function and DOLP for unpolarized incident light
+# scattered by a large spherical particle with m=1.33 and size parameter X = 500
+
